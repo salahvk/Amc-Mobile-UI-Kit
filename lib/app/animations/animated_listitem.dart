0d3c336ea0e244d42d9a76e27a6a19413221acdb
salahvk <salahkoyilandy@gmail.com>
2025-05-20 12:05:45 +0530

    add animated list item and fade transition widget

---
+++ lib/app/animations/animated_listitem.dart
@@ -0,0 +1,35 @@
+import 'package:amc_ui_kit/app/animations/fade_transition_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+
+
+class AnimatedListItem extends StatelessWidget {
+  final int index;
+  final Widget child;
+  final Duration duration;
+  final double verticalOffset;
+
+  const AnimatedListItem({
+    super.key,
+    required this.index,
+    required this.child,
+    this.duration = const Duration(milliseconds: 500),
+    this.verticalOffset = 50.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimationConfiguration.staggeredList(
+      position: index,
+      duration: duration,
+      child: SlideAnimation(
+        verticalOffset: verticalOffset,
+        child: FadeInAnimation(
+          child: FadeTransitionWidget(
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app/animations/fade_transition_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class FadeTransitionWidget extends StatefulWidget {
+  final Widget child;
+
+  const FadeTransitionWidget({super.key, required this.child});
+
+  @override
+  State<FadeTransitionWidget> createState() => _FadeTransitionWidgetState();
+}
+
+class _FadeTransitionWidgetState extends State<FadeTransitionWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacity;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _opacity = Tween<double>(begin: 0, end: 1).animate(_controller);
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _opacity,
+      child: widget.child,
+    );
+  }
+}
